52bf12752a73ab3766ac4df43551a095543c6777
vanelizondo <vnelizondo89@mail.fresnostate.edu>
2020-05-09 17:14:05 -0700

    finished Lab Assignment 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ToastService } from './toast/toast.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  toastTypes: Array<string> = [];
+  constructor(private toastService: ToastService) { 
+    this.toastTypes = ['success', 'info', 'warning', 'danger'];
+  }
+
+  // success, info, warning, danger
+  showToast() {
+    const rand = Math.floor(Math.random() * 4);
+    const toastType = this.toastTypes[rand];
+    const toastMessage = 'Hi This is a message my random is :' + rand;
+    const duration = 4000;
+    this.toastService.showToast(toastType, toastMessage, duration);
+  }
 }
